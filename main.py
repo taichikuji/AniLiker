7912e6d839c707041930861f5b13f5e7f7ea0540
Iván Pérez <48720361+taichikuji@users.noreply.github.com>
2021-05-13 07:53:49 +0200

    PR from Dev to Master (#4)
    * Add check for X-RateLimit-Remaining
    
    This is currently not working, hence why I created a new branch instead of pushing to main.
    
    The objective is that I'll be able to check dynamically if the rate limit is hit before an error occurs, and this way evade API X-RateLimit errors.
    Changes to be committed:
        * modified:   main.py
    
    * Edit workflow
    Changes to be committed:
        * new file:   .github/linters/.flake8
        * modified:   .github/workflows/ codeql-analysis.yml
        * modified:   Pipfile
        * modified:   Pipfile.lock
        * modified:   main.py
        * modified:   utils/oauth.py
    
    * Edit workflow name
    Changes to be committed:
     * renamed:    .github/workflows/ codeql-analysis.yml -> .github/workflows/linter.yml
    
    * Lint main.py
    
    * Lint Main.py
    Changes to be committed:
        * modified:   main.py
    
    * ISORT is a [***]
    
    * Fix ISORT linting issue
    
    * Remove check for X-RateLimit-Remaining
    
    * Remove unused import [F401]

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-import requests
-from time import sleep
 import os
-from os.path import join, dirname
+from os.path import dirname, join
+from time import sleep
+
+import requests
 from dotenv import load_dotenv, set_key
 from utils import oauth
 
-dotenv_path = join(dirname(__file__), '.env')
+dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 
 AL_DATA = {}
-if ((os.environ.get("ANILIST_TOKEN") == None) or (os.environ.get("ANILIST_TOKEN") == "")):
+if (os.environ.get("ANILIST_TOKEN") is None) or (os.environ.get("ANILIST_TOKEN") == ""):
     AL_DATA = {
         "ANILIST_CLIENT_ID": os.environ.get("ANILIST_CLIENT_ID"),
         "ANILIST_CLIENT_SECRET": os.environ.get("ANILIST_CLIENT_SECRET"),
-        "ANILIST_REDIRECT_URI": os.environ.get("ANILIST_REDIRECT_URI")
+        "ANILIST_REDIRECT_URI": os.environ.get("ANILIST_REDIRECT_URI"),
     }
-    AL_DATA["ANILIST_TOKEN"] = oauth.GET_AL_TOKEN(AL_DATA)['access_token']
-    set_key(dotenv_path, "ANILIST_TOKEN",
-            AL_DATA["ANILIST_TOKEN"], quote_mode="always")
+    AL_DATA["ANILIST_TOKEN"] = oauth.GET_AL_TOKEN(AL_DATA)["access_token"]
+    set_key(dotenv_path, "ANILIST_TOKEN", AL_DATA["ANILIST_TOKEN"], quote_mode="always")
     print("AL Token has been saved on dotenv file")
 else:
     AL_DATA["ANILIST_TOKEN"] = os.environ.get("ANILIST_TOKEN")
@@ -28,9 +28,9 @@ def run_query(query, variables):
         "https://graphql.anilist.co",
         json={"query": query, "variables": variables},
         headers={
-            'content-type': "application/json",
-            'authorization': "Bearer " + AL_DATA["ANILIST_TOKEN"]
-        }
+            "content-type": "application/json",
+            "authorization": "Bearer " + AL_DATA["ANILIST_TOKEN"],
+        },
     )
 
     print(response)
@@ -49,24 +49,22 @@ def main():
 
     # Get user Id based on username
 
-    query = '''
+    query = """
         query ($username: String) {
           User (name: $username) {
             id
           }
         }
-    '''
+    """
 
-    variables = {
-        "username": ANILIST_USERNAME
-    }
+    variables = {"username": ANILIST_USERNAME}
 
     user_id = run_query(query, variables)["User"]["id"]
 
     # Get latest AniList activities by user Id.
     npage = 1
     while npage > 0:
-        query = '''
+        query = """
       query ($user_id: Int, $page: Int, $perPage: Int) {
               Page (page: $page, perPage: $perPage) {
                 pageInfo {
@@ -85,29 +83,22 @@ def main():
                 }
               }
             }
-      '''
+      """
 
-        variables = {
-            "user_id": user_id,
-            "page": npage,
-            "perPage": 30
-
-        }
+        variables = {"user_id": user_id, "page": npage, "perPage": 30}
         page = run_query(query, variables)["Page"]
         activity = page["activities"]
         pageInfo = page["pageInfo"]["hasNextPage"]
 
         for value in activity:
-            query = '''
+            query = """
           mutation ($id: Int) {
             ToggleLikeV2(id: $id, type: ACTIVITY) {
               __typename
             }
           }
-        '''
-            variables = {
-                "id": value["id"]
-            }
+        """
+            variables = {"id": value["id"]}
 
             # ToggleLikeV2 runs
             run_query(query, variables)
--- utils/oauth.py
+++ utils/oauth.py
@@ -1,15 +1,17 @@
-import requests
 from requests_oauthlib import OAuth2Session
 
 
 def GET_AL_TOKEN(DATA):
     client_id = DATA["ANILIST_CLIENT_ID"]
     client_secret = DATA["ANILIST_CLIENT_SECRET"]
-    authorization_base_url = 'https://anilist.co/api/v2/oauth/authorize'
-    token_url = 'https://anilist.co/api/v2/oauth/token'
+    authorization_base_url = "https://anilist.co/api/v2/oauth/authorize"
+    token_url = "https://anilist.co/api/v2/oauth/token"
     AL = OAuth2Session(client_id, redirect_uri=DATA["ANILIST_REDIRECT_URI"])
     authorization_url, state = AL.authorization_url(authorization_base_url)
     redirect_response = input(
-        f"Your Oauth2 url is {authorization_url} .\n Visit the url and login, then paste the full redirect URL here\n==============\nIF YOU GET A INSECURE HTTP ERROR, REPLACE HTTP WITH HTTPS\n==============\n> ")
+        f"Your Oauth2 url is {authorization_url} .\n Visit the url and login, then paste the full redirect URL here\n==============\nIF YOU GET A INSECURE HTTP ERROR, REPLACE HTTP WITH HTTPS\n==============\n> "
+    )
 
-    return AL.fetch_token(token_url, client_secret=client_secret, authorization_response=redirect_response)
+    return AL.fetch_token(
+        token_url, client_secret=client_secret, authorization_response=redirect_response
+    )
